0ba7186c38c05660648b2310fe7540dab5a1ef35
Alex <7zaytsev00@mail.ru>
2024-04-16 21:08:20 +0700

    FixParser

--- MenuStrip/Parsers/ParserMenu.cs
+++ MenuStrip/Parsers/ParserMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MenuStrip.Parsers
@@ -24,8 +25,38 @@ namespace MenuStrip.Parsers
 
             using (StreamReader streamReader = new StreamReader(_pathConfig))
             {
-                while (!streamReader.EndOfStream) { strings.Add(streamReader.ReadLine().Split(' ')); }
+                while (!streamReader.EndOfStream) { 
+                    string line = streamReader.ReadLine();
+                    string pattern = @"^(\d+) (.+) (\d+) (.+)$";
+                    string pattern2 = @"^(\d+) (.+) (\d+)$";
+
+                    if (Regex.IsMatch(line, pattern))
+                    {
+                        foreach (Match match in Regex.Matches(line, pattern))
+                        {
+                            strings.Add(new string[] {
+                        match.Groups[1].Value,
+                        match.Groups[2].Value,
+                        match.Groups[3].Value,
+                        match.Groups[4].Value
+                        });
+                        }
+                    }
+                    if (Regex.IsMatch(line, pattern2)) {
+
+                        foreach (Match match in Regex.Matches(line, pattern2))
+                        {
+                            strings.Add(new string[] {
+                        match.Groups[1].Value,
+                        match.Groups[2].Value,
+                        match.Groups[3].Value
+                        });
+                        }
+                    }
+                }
             }
+
+
 
             return strings;
         }
